027330ec58d9ef013799b70206b564e2cdef21cb
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-09 14:10:28 +0300

    підключення розмітки

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+var debounce = require('lodash.debounce');
+
+const inputRef = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
