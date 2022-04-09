3600aa2b256503c97897bc2072c1925967b502a5
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-09 16:55:36 +0300

    fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './css/styles.css';
+var debounce = require('lodash.debounce');
+import { fetchCountries } from './js/fetchCountries' ;
 
 const DEBOUNCE_DELAY = 300;
-
-var debounce = require('lodash.debounce');
 
 const inputRef = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
+inputRef.addEventListener('input', debounce(inputCountry, DEBOUNCE_DELAY));
+
+inputCountry(val){
+    fetchCountries(nameInput)
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,12 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const option = '?fields=name,capital,population,flags,languages';
+
 export function fetchCountries(name) {
-    
+    fetch(`${BASE_URL}${name}${option}`)
+        .then(response => {
+            if (!response.ok) {
+                throw Error(response.status);
+            }
+            return response.json();
+        });
 }
