18f0306e7b6ab43db353009b9b807f5ef2b6c6d1
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-09 20:55:26 +0300

    розмітка однієї країни

--- src/index.js
+++ src/index.js
@@ -12,9 +12,12 @@ const countryInfo = document.querySelector('.country-info');
 inputRef.addEventListener('input', debounce( inputCountry, DEBOUNCE_DELAY ));
 
 function inputCountry(val) {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
     const nameInput = val.target.value.trim();
+
+    if (!nameInput) {
+        clearContent()
+        return
+    }
 
     fetchCountries(nameInput)
         .then(createCountryInfo)
@@ -24,24 +27,40 @@ function inputCountry(val) {
 function createCountryInfo(data) {
     if (data.length > 10) {
         Notify.info('Too many matches found. Please enter a more specific name.');
-        return
+        return;
     }
 
     if (data.length === 1) {
-        oneCountyInfo(data, option)
-        return
+        oneCountyInfo(data)
+        return;
     }
     
     markupCountriesList(data)
 }
 
-function createError() {
-    Notify.failure('Oops, there is no country with that name')
-    return
+function oneCountyInfo(data) {
+    const { name, capital, population, flags } = data[0];
+    const languages = Object.values(data[0].languages).join('; ');
+
+    countryList.innerHTML = '';
+
+    countryInfo.innerHTML = `
+    <img src="${flags.svg}" alt="${name.official}"
+    width="60px" height="40px">
+    <p class="county-name"> ${name.official} </p>
+    <ul class="one-country__list">
+        <li class="one-country__item"> Capital: ${capital}
+        </li>
+        <li class="one-country__item"> Population: ${population} people
+        </li>
+        <li class="one-country__item"> Languages: ${languages}
+        </li>
+    </ul>
+    `
 }
 
 function markupCountriesList(data) {
-    countryInfo.textContent = '';
+    countryInfo.innerHTML = '';
 
     const create = data.map(({ flags, name }) => `
     <li class="country-item"> 
@@ -52,4 +71,14 @@ function markupCountriesList(data) {
     `,).join('');
 
     countryList.innerHTML = create;
+}
+
+function createError() {
+    Notify.failure('Oops, there is no country with that name')
+    return
+}
+
+function clearContent() {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
 }
