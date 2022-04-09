a5097aa8a129c2923e9d3f28c6fe8b2215397c6a
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-04-09 17:12:36 +0300

    установка библиотеки Notify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 var debounce = require('lodash.debounce');
-import { fetchCountries } from './js/fetchCountries' ;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
